2a2e1f7abee0cc8c069004593ffc388e37013187
Carlos A. Guevara <carlos.guevara@gaiadesign.com.mx>
2020-07-15 08:19:10 -0500

    jinja templates

--- pypisite_com/app.py
+++ pypisite_com/app.py
@@ -3,9 +3,23 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.3'},
+        {'name': 'passlib', 'version': '5.2.3'}
+    ]
+
+
 @app.route('/')
 def index():
-    return "Hello world"
+    test_packages = get_latest_packages()
+    return flask.render_template('home/index.html', packages=test_packages)
 
 
-app.run()
+@app.route('/about')
+def about():
+    return flask.render_template('home/about.html')
+
+if __name__ == '__main__':
+    app.run()
